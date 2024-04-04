6d7e73c804b9684769ab510317956a237a103e3d
Vadim <91436203+VadimWictorovich@users.noreply.github.com>
2024-04-04 22:03:08 +0300

    project created

---
+++ UnoSoftTestWork/UnoSoftTestWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnoSoftTestWork
+//
+//  Created by Вадим Игнатенко on 4.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
